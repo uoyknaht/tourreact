1be5e1eb1f2f8f26b90eb7ada03203b63129b1b2
Paulius R <paulius.repsys@gmail.com>
2015-08-07 09:48:03 +0300

    Made redirect after saving place

--- public/src/js/components/placeFormComponent.js
+++ public/src/js/components/placeFormComponent.js
@@ -1,43 +1,54 @@
 import React from 'react';
+import Router from 'react-router';  
 
 export default class PlaceForm extends React.Component {
 
     constructor() {
         super();
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleCancel = this.handleCancel.bind(this);
         this.render = this.render.bind(this);
     }
 
     handleSubmit(e) {
         e.preventDefault();
 
-        var title = React.findDOMNode(this.refs.title).value.trim();
-        var address = React.findDOMNode(this.refs.address).value.trim();
-        var latitude = React.findDOMNode(this.refs.latitude).value.trim();
-        var longitude = React.findDOMNode(this.refs.longitude).value.trim();
+        var data = {
+            title: React.findDOMNode(this.refs.title).value.trim(),
+            address: React.findDOMNode(this.refs.address).value.trim(),
+            latitude: React.findDOMNode(this.refs.latitude).value.trim(),
+            longitude: React.findDOMNode(this.refs.longitude).value.trim()
+        }
 
-        this.props.onPlaceSubmit({
-            title: title, 
-            address: address,
-            latitude: latitude,
-            longitude: longitude
-        }); 
+
+        this.props.onPlaceSubmit(data, this.props.isEditAction); 
 
         React.findDOMNode(this.refs.title).value = '';
         React.findDOMNode(this.refs.address).value = '';
         React.findDOMNode(this.refs.latitude).value = '';
         React.findDOMNode(this.refs.longitude).value = '';
 
-        return;
+        this._redirectAfterSave();
     }
 
+    handleCancel(e) {
+        e.preventDefault();
+        this._redirectAfterSave();
+    }  
+
+    _redirectAfterSave() {
+        this.context.router.transitionTo('allPlaces');
+    }  
+
     render() {
+
+        var title = this.props.isEditAction ? 'Add new place' ? 'Edit place';
 
         return (
             <div className="dynamic-menu">
                 <div className="row">
                     <form className="placeForm" class="col s12">
-                        <h2>Add new place</h2>
+                        <h2>{title}</h2>
                         <div className="row">
                             <div class="input-field col s12">
                                 <input type="text"  ref="title" id="place-form-title" placeholder="Title" className="validate" />
@@ -70,8 +81,8 @@ export default class PlaceForm extends React.Component {
                         <br/>
                         <div className="row">
                             <div class="input-field col s12">
-                                <input type="button" className="waves-effect waves-light btn" type="submit" value="Save" onClick={this.handleSubmit} />
-                                <a className="waves-effect waves-light btn">Cancel</a>
+                                <input type="submit" className="waves-effect waves-light btn" value="Save" onClick={this.handleSubmit} />
+                                <button type="button" className="waves-effect waves-light btn" onClick={this.handleCancel}>Cancel</button>
                             </div>
                         </div>
 
@@ -82,3 +93,8 @@ export default class PlaceForm extends React.Component {
     }
 
 }
+
+// https://github.com/rackt/react-router/issues/975
+PlaceForm.contextTypes = {
+  router: React.PropTypes.func.isRequired
+};
